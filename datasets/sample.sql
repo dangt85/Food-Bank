d71e295f970143137f6b09173920afc138d2bb30
Daniel Gonzalez <daniel.gonzalez@codescience.com>
2022-08-19 12:03:57 -0400

    Add demo dataset

---
+++ datasets/sample.sql
@@ -0,0 +1,28 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES(2,'Acme Corp.','');
+CREATE TABLE "Delivery_Item__c" (
+	id INTEGER NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES(1,'2022-09-16','Refrigerated','Acme Corp - Produce - Summer 2022','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2023-01-20','Non-refrigerated','Acme Corp - Canned Vegetables - Summer 2022','1');
+CREATE TABLE "Delivery__c" (
+	id INTEGER NOT NULL, 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES(1,'2022-08-19T16:00:00.000+0000','Requested','2');
+COMMIT;
